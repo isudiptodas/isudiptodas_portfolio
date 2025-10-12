e6b8bbbc174dfc9fad0b8707e4f1555d9322d0c2
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-10-13 00:04:16 +0530

    Added initial design (explore)

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -4,7 +4,9 @@ function page(){
 
   return (
     <>
-    
+      <div className={`w-full h-screen bg-[#111111] overflow-y-hidden flex flex-col justify-start items-center pt-5`}>
+        <h1 className={`text-[12px] font-Michroma w-full text-white text-center md:text-sm`}>Coming Soon</h1>
+      </div>
     </>
   )
 
