b34a8dfe4ef145d4156d4bba1fc81f39aee5a078
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-10-31 21:29:28 +0530

    Updated image list

--- src/data/work.ts
+++ src/data/work.ts
@@ -1,4 +1,6 @@
 export const imageList = [
+'https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/photography/denver-1.jpg',
+'https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/photography/denver-2.jpg',
 'https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/photography/dogs.jpg',
 'https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/photography/sleeping-cat-1.jpg',
 'https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/photography/sleeping-cat-2.jpg',
@@ -58,6 +60,7 @@ export const designList = [
 'https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/design_work/the-weeknd.png',
 'https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/design_work/zayn.png',
 ];
+
 
 
 
