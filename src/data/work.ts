73ed79d54db9cf7ce793faf5b1a03b9b0be9a5c0
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-11-02 04:28:24 +0530

    Updated design links

--- src/data/work.ts
+++ src/data/work.ts
@@ -43,6 +43,9 @@ export const imageList = [
 ];
 
 export const designList = [
+'https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/design_work/srk-1.png',
+'https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/design_work/srk-2.png',
+'https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/design_work/srk-3.png',
 'https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/design_work/stranger-things-5.jpg',
 'https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/design_work/argentina.png',
 'https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/design_work/beats-solo.png',
@@ -60,6 +63,7 @@ export const designList = [
 'https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/design_work/the-weeknd.png',
 'https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/design_work/zayn.png',
 ];
+
 
 
 
