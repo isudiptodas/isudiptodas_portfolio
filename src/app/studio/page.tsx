480ea465d08b2be97238a23e13dfee2fae822774
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-11-06 00:36:33 +0530

    added video section in studio

--- src/app/studio/page.tsx
+++ src/app/studio/page.tsx
@@ -3,12 +3,16 @@
 import { designList, imageList } from "@/data/work";
 import { useState } from "react";
 import Link from 'next/link';
+import { videoLinks } from "@/data/video";
+import { FaPlay } from "react-icons/fa";
 
 function page() {
 
-  const [option, setOption] = useState('photo');
+  const [option, setOption] = useState('video');
+  const[videoOpen, setVideoOpen] = useState(false);
+  const[currentVideo, setCurrentVideo] = useState<null | string>(null);
 
-  const openLink = (link:string) => {
+  const openLink = (link: string) => {
     window.open(link, '_blank');
   }
 
@@ -23,24 +27,41 @@ function page() {
           <p onClick={() => setOption('video')} className={`w-auto ${option === 'video' ? "bg-[#B2FF6E] text-black font-bold" : "bg-transparent text-white"} rounded-full py-3 px-3 text-[10px] md:text-sm xl:text-lg cursor-pointer tracking-widest font-Michroma`}>Video</p>
         </div>
 
-        <div className={`w-full h-auto flex justify-center items-center px-5 fixed bottom-8`}>
-        <Link href='/' className={`w-auto px-5 py-3 rounded-full text-white font-Michroma text-[10px] md:text-sm backdrop-blur-3xl bg-white/25`}>Exit Studio</Link>
+        {/* video play box */}
+        <div className={`w-full ${videoOpen ? "block" : "hidden"} px-5 pt-10 h-screen flex flex-col justify-start items-center gap-5 top-0 fixed bg-black z-50`}>
+          <div className={`w-auto h-[70vh] md:h-[75vh] md:w-1/2 bg-black overflow-hidden rounded-lg`}>
+            {currentVideo && <video src={currentVideo as string} className={`h-full w-full`} autoPlay loop muted controls/>}
+          </div>
+          <p onClick={() => {setVideoOpen(false); setCurrentVideo(null)}} className={`w-auto active:opacity-75 duration-150 ease-in-out px-4 text-center py-2 rounded-full text-black bg-white cursor-pointer text-sm md:text-lg`}>Close Video</p>
         </div>
 
-        {option === 'photo' && <div className={`w-[90%] h-auto py-5 mt-36 lg:px-3 bg-[#111111] columns-2 md:columns-3 lg:columns-4 overflow-auto`}>
+        <div className={`w-full ${videoOpen ? "hidden" : "block"} h-auto z-50 flex justify-center items-center px-5 fixed bottom-8`}>
+          <Link href='/' className={`w-auto px-5 py-3 rounded-full text-white font-Michroma text-[10px] md:text-sm backdrop-blur-3xl bg-white/25`}>Exit Studio</Link>
+        </div>
+
+        {option === 'photo' && <div className={`w-[90%] h-auto py-5 mt-36 lg:mt-40 lg:px-3 bg-[#111111] columns-2 md:columns-3 lg:columns-4 overflow-auto`}>
           {imageList.map((image, index) => {
             return <img onClick={() => openLink(image)} key={index} src={image} className={`h-auto cursor-pointer my-5 rounded-lg break-inside-avoid`} />
           })}
         </div>}
 
-        {option === 'design' && <div className={`w-[90%] h-auto py-5 mt-36 lg:p bg-[#111111] columns-2 md:columns-3 lg:columns-4 overflow-auto`}>
+        {option === 'design' && <div className={`w-[90%] h-auto py-5 mt-36 lg:mt-40 lg:p bg-[#111111] columns-2 md:columns-3 lg:columns-4 overflow-auto`}>
           {designList.map((design, index) => {
             return <img onClick={() => openLink(design)} key={index} src={design} className={`h-auto cursor-pointer my-5 rounded-lg break-inside-avoid`} />
           })}
         </div>}
 
-        {option === 'video' && <div className={`w-[90%] h-auto py-5 px-2 mt-36 lg:px-5 bg-[#111111] flex flex-col justify-start items-center overflow-auto`}>
-          <p className={`w-full text-center font-Michroma text-[10px] md:text-lg lg:text-4xl font-bold text-white tracking-widest`}>Coming Soon</p>
+        {option === 'video' && <div className={`w-[90%] lg:w-[95%] h-auto py-5 px-2 mt-36 lg:mt-40 lg:px-5 bg-[#111111] flex flex-col justify-start items-center overflow-auto`}>
+          <h1 className={`font-Michroma mt-3 border-b-[1px] border-gray-400 pb-5 text-lg font-bold text-white w-full text-start`}>Video Edits</h1>
+
+          <div className={`w-full h-[30vh] md:h-[40vh] lg:h-[60vh] py-4 flex items-center gap-4 overflow-x-auto scroll-smooth snap-x snap-mandatory`}>
+            {videoLinks.map((link, index) => {
+              return <div key={index} className={`shrink-0 snap-center w-auto h-full flex justify-center relative items-center overflow-hidden rounded-2xl bg-neutral-800`}>
+                <img src={link.thumbnail as string} className={`h-full w-auto object-cover`} />
+                <span onClick={() => {setVideoOpen(true); setCurrentVideo(link.link as string)}} className={`absolute p-2 md:p-4 xl:p-6 group rounded-full bg-white cursor-pointer shadow-2xl`}><FaPlay className={`group-hover:text-blue-500 duration-200 ease-in-out`}/></span>
+              </div>
+            })}
+          </div>
         </div>}
 
       </div>
---
+++ src/data/video.ts
@@ -0,0 +1,15 @@
+export const videoLinks = [
+    {
+        link: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/video/bees_and_nature.mp4`,
+        thumbnail: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/thumbnail/bees-and-nature.png`
+    },
+    {
+        link: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/video/how_internet_works.mp4`,
+        thumbnail: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/thumbnail/how-internet-works.png`
+    },
+    {
+        link: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/video/rise_of_microsoft.mp4`,
+        thumbnail: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/thumbnail/rise-of-microsoft.png`
+    },
+ 
+]
