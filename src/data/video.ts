b6cad570e5268a46ebd9b6c4157c818c256c1c67
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-12-17 15:54:14 +0530

    Update video.ts

--- src/data/video.ts
+++ src/data/video.ts
@@ -11,5 +11,9 @@ export const videoLinks = [
         link: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/video/rise_of_microsoft.mp4`,
         thumbnail: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/thumbnail/rise-of-microsoft.png`
     },
- 
+    {
+        link: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/video/mother_earth.mp4`,
+        thumbnail: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/thumbnail/mother-earth.png`
+    },
+
 ]
