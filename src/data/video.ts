fcb88520b240b48e6fba8760071f0eb60ed6e527
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-12-17 16:21:21 +0530

    Update video.ts

--- src/data/video.ts
+++ src/data/video.ts
@@ -1,5 +1,9 @@
 export const videoLinks = [
     {
+        link: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/video/mother_earth.mp4`,
+        thumbnail: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/thumbnail/mother-earth.png`
+    },
+    {
         link: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/video/bees_and_nature.mp4`,
         thumbnail: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/thumbnail/bees-and-nature.png`
     },
@@ -11,9 +15,6 @@ export const videoLinks = [
         link: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/video/rise_of_microsoft.mp4`,
         thumbnail: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/thumbnail/rise-of-microsoft.png`
     },
-    {
-        link: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/video/mother_earth.mp4`,
-        thumbnail: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/thumbnail/mother-earth.png`
-    },
+   
+];
 
-]
