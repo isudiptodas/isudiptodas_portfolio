ee4b2c926308b55e03f65eb8e149d2edd2a68b8d
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2026-01-08 01:31:05 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,8 +57,8 @@ function page() {
         <div className={`w-full h-screen z-10 bg-[#080808] flex flex-col justify-center items-center relative overflow-hidden gap-3`}>
           <p className={`absolute left-1/2 -translate-x-1/2 top-7 font-Michroma text-[10px] text-white tracking-[5px]`}>ISUDIPTODAS</p>
 
-          <h1 className={`text-white text-start font-Cronde italic text-4xl sm:text-7xl w-full pl-12 md:pl-16 lg:pl-44 xl:pl-56 overflow-hidden`}>DESIGNER</h1>
-          <h1 className={`text-white text-end font-Zen-Dots text-3xl w-full sm:text-6xl pr-12 md:pr-16 lg:pr-44 xl:pr-56 overflow-hidden`}>DEVELOPER</h1>
+          <h1 className={`text-white text-center font-Cronde italic text-4xl sm:text-7xl w-full overflow-hidden`}>DESIGNER</h1>
+          <h1 className={`text-white text-center font-Zen-Dots text-3xl w-full sm:text-6xl overflow-hidden`}>DEVELOPER</h1>
 
           <div className={`w-full absolute bottom-0 py-8 md:py-12 px-5 grid grid-cols-2 sm:grid-cols-4 justify-items-center gap-3`}>
             <p className={`text-white font-Michroma text-[8px] md:text-[10px] lg:text-sm cursor-pointer`}>WEB DEVELOPMENT</p>
@@ -267,6 +267,7 @@ function page() {
 }
 
 export default page
+
 
 
 
