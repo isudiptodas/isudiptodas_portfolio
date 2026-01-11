c2cddae62f98dda526aa1f92951e43c789c733ab
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2026-01-11 20:18:28 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -249,7 +249,7 @@ function page() {
           <h1 className={`w-full text-center font-Michroma font-bold text-black text-xl md:text-2xl`}>WHAT PEOPLE SAY</h1>
           <p className={`w-full flex justify-center items-center text-[10px] md:text-[12px] gap-2 mt-2 mb-5`}> <span className="text-yellow-500 text-[14px]"><SiGooglegemini /></span> Read out feedback from my network <span className="text-yellow-500 text-lg"><SiGooglegemini /></span></p>
 
-          <div className={`w-full h-auto flex flex-col justify-center items-center relative overflow-hidden pb-5`}>
+          <div className={`w-full h-auto flex flex-col justify-center items-center relative overflow-hidden pb-5 pt-5`}>
             <div className="h-full z-20 absolute left-0 w-[20%] lg:w-[30%] bg-gradient-to-r from-white to-transparent"></div>
 
             <Marquee>
@@ -312,6 +312,7 @@ function page() {
 }
 
 export default page
+
 
 
 
