dd353e9b42bfd873d1a08456417462223515485e
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2026-01-11 17:30:49 +0530

    Update testimonials.ts

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -6,7 +6,8 @@ export const testimonials = [
                Along with development, he’s also a really good designer with a clean and well-structured design sense. His work in editing, including video editing, 
                is neat and professional as well. Overall, he’s responsible, dependable, and someone who takes up work seriously and delivers it on time.`,
     linkedin: "https://www.linkedin.com/in/pritam--paul",
-    designation: "SAP Inside Track Kolkata Organiser, CSE Final Year Student - SNU Kolkata"
+    designation: "SAP Inside Track Kolkata Organiser, CSE Final Year Student - SNU Kolkata",
+    image: `/assets/testimonials/pritam-paul.jpg`
   },
   {
     name: "Tanuja Chowdhury",
@@ -15,7 +16,8 @@ export const testimonials = [
                approach is logical, his design sense is clean and well structured, and his editing work, including video editing, is neat and professional. 
                He is reliable with deadlines, takes responsibility, communicates clearly, and is easy to collaborate with, always supporting the team when needed.`,
     linkedin: "https://www.linkedin.com/in/tanuja-chowdhury-95b599320",
-    designation: "SAP Inside Track Kolkata Marketing Lead, CSE Final Year Student - SNU Kolkata"
+    designation: "SAP Inside Track Kolkata Marketing Lead, CSE Final Year Student - SNU Kolkata",
+    image: `/assets/testimonials/tanuja-chowdhury.jpg`
   },
   {
     name: "Areeba Begum",
@@ -23,7 +25,8 @@ export const testimonials = [
                and attention to detail into everything he works on. His positive attitude, willingness to support others, and consistent on-time delivery make him a 
                pleasure to work with and a valuable part of any team.`,
     linkedin: "https://www.linkedin.com/in/areeba-begum",
-    designation: "CSE Final Year Student - SNU Kolkata"
+    designation: "CSE Final Year Student - SNU Kolkata",
+    image: `/assets/testimonials/areeba-begum.jpg`
   },
   {
     name: "Pritam Kumar Mani",
@@ -31,13 +34,15 @@ export const testimonials = [
                turnaround of just two days. Despite being introduced through a mutual connection at the last minute, Sudipto not only met the deadline but 
                delivered the work ahead of schedule. His commitment to quality and efficiency is impressive, and I have no doubt he will achieve great success in his career.`,
     linkedin: "https://www.linkedin.com/in/pkumarmani24",
-    designation: "Application Developer, IBM"
+    designation: "Application Developer, IBM",
+    image: `/assets/testimonials/pritam-kumar-mani.jpg`
   },
   {
     name: "Debaleena Dutta",
     feedback: `Sudipto has a strong ability of dealing with every technical problem he faces. He's a great listener and understands tiny bits of the job given to him. 
                Being his mentor, I got a lot of positive vibes from him, and he always tried to fulfill the requirements in every possible way. Hope he shines bright imminently.`,
     linkedin: "https://www.linkedin.com/in/dr-debaleena-datta-33b25494",
-    designation: "Assistant Professor, Techno India"
+    designation: "Assistant Professor, Techno India",
+    image: `/assets/testimonials/debaleena-dutta.jpg`
   }
 ];
