0dd95cb1154cbac39c88c67d90ea5e392e20397d
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2026-01-24 01:09:50 +0530

    Create techStack.ts

---
+++ src/data/techStack.ts
@@ -0,0 +1,34 @@
+export const techStackList = [
+  {
+    title: "Programming Languages",
+    items: "C++, Java",
+  },
+  {
+    title: "Frontend",
+    items: "HTML, CSS, JavaScript, TypeScript, TailwindCSS, ReactJs, NextJs",
+  },
+  {
+    title: "Backend",
+    items: "NodeJs, ExpressJs, SpringBoot, Supabase",
+  },
+  {
+    title: "Design & Modeling",
+    items: "Canva, Figma, Affinity, Spline",
+  },
+  {
+    title: "Databases",
+    items: "MySQL, MongoDB, Datastax, NeonDB, Redis",
+  },
+  {
+    title: "Other Tools",
+    items: "ArcJet, Drizzle, Prisma",
+  },
+  {
+    title: "Cloud",
+    items: "Docker, AWS S3, AWS EC2, DynamoDB, SES, IAM",
+  },
+  {
+    title: "Messaging Queues",
+    items: "BullMQ",
+  },
+];
