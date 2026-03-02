d734f718a6482bd238a7a461ba34ba26339094a4
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2026-03-02 14:40:56 +0530

    Update documents.ts

--- src/data/documents.ts
+++ src/data/documents.ts
@@ -1,24 +1,25 @@
 export const documentList = [
     {
         name: `Vibe Error`,
-        thumbnail: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/documents_thumbnail/vibe-error.png`,
-        link: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/documents/Vibe%20Error.pdf`
+        thumbnail: `https://ik.imagekit.io/isudiptodas/document_thumbnail/vibe-error.png`,
+        link: `https://ik.imagekit.io/isudiptodas/documents/Vibe%20Error.pdf`
     },
     {
         name: `The Art Of Web Development`,
-        thumbnail: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/documents_thumbnail/web-development.png`,
-        link: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/documents/The%20Art%20Of%20Web%20Development.pdf`
+        thumbnail: `https://ik.imagekit.io/isudiptodas/document_thumbnail/web-development.png`,
+        link: `https://ik.imagekit.io/isudiptodas/documents/The%20Art%20Of%20Web%20Development.pdf`
     },
     {
         name: `Decoding LLM`,
-        thumbnail: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/documents_thumbnail/decoding-llm.png`,
-        link: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/documents/Decoding%20LLM.pdf`
+        thumbnail: `https://ik.imagekit.io/isudiptodas/document_thumbnail/decoding-llm.png`,
+        link: `https://ik.imagekit.io/isudiptodas/documents/Decoding%20LLM.pdf`
     },
     {
         name: `The Gemini Effect`,
-        thumbnail: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/documents_thumbnail/gemini-effect.png`,
-        link: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/documents/The%20Gemini%20Effect.pdf`
+        thumbnail: `https://ik.imagekit.io/isudiptodas/document_thumbnail/gemini-effect.png`,
+        link: `https://ik.imagekit.io/isudiptodas/documents/The%20Gemini%20Effect.pdf`
     },
 
 ]
+
 
