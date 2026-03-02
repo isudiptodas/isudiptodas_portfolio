5d11cdf3cbbcee49bdc1924883a9f8f8be98aeea
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2026-03-02 14:33:49 +0530

    Update video.ts

--- src/data/video.ts
+++ src/data/video.ts
@@ -1,30 +1,31 @@
 export const videoLinks = [
     {
-        link: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/video/save_aravalli.mp4`,
-        thumbnail: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/thumbnail/save-aravalli.png`
+        link: `https://ik.imagekit.io/isudiptodas/video/save_aravalli.mp4`,
+        thumbnail: `https://ik.imagekit.io/isudiptodas/video_thumbnail/save-aravalli.png`
     },
     {
-        link: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/video/thinking_is_optional.mp4`,
-        thumbnail: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/thumbnail/thinking-is-optional.png`
+        link: `https://ik.imagekit.io/isudiptodas/video/thinking_is_optional.mp4`,
+        thumbnail: `https://ik.imagekit.io/isudiptodas/video_thumbnail/thinking-is-optional.png`
     },
     {
-        link: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/video/mother_earth.mp4`,
-        thumbnail: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/thumbnail/mother-earth.png`
+        link: `https://ik.imagekit.io/isudiptodas/video/mother_earth.mp4`,
+        thumbnail: `https://ik.imagekit.io/isudiptodas/video_thumbnail/mother-earth.png`
     },
     {
-        link: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/video/bees_and_nature.mp4`,
-        thumbnail: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/thumbnail/bees-and-nature.png`
+        link: `https://ik.imagekit.io/isudiptodas/video/bees_and_nature.mp4`,
+        thumbnail: `https://ik.imagekit.io/isudiptodas/video_thumbnail/bees-and-nature.png`
     },
     {
-        link: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/video/how_internet_works.mp4`,
-        thumbnail: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/thumbnail/how-internet-works.png`
+        link: `https://ik.imagekit.io/isudiptodas/video/how_internet_works.mp4`,
+        thumbnail: `https://ik.imagekit.io/isudiptodas/video_thumbnail/how-internet-works.png`
     },
     {
-        link: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/video/rise_of_microsoft.mp4`,
-        thumbnail: `https://lfjakrpqbidggevkatwm.supabase.co/storage/v1/object/public/thumbnail/rise-of-microsoft.png`
+        link: `https://ik.imagekit.io/isudiptodas/video/rise_of_microsoft.mp4`,
+        thumbnail: `https://ik.imagekit.io/isudiptodas/video_thumbnail/rise-of-microsoft.png`
     },
    
 ];
+
 
 
 
