c3af3ccd10502380da0e2e1b96169ed3e7f37658
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2026-03-06 20:32:30 +0530

    Update page.tsx

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -72,7 +72,7 @@ function page() {
                 </div>
 
                 <div className={`w-full px-5 lg:px-8 mt-20 flex justify-start lg:justify-center items-center gap-4`}>
-                    <input onChange={(e) => setSearchTerm(e.target.value)} type="text" className={`w-[70%] lg:w-[50%] py-2 px-3 bg-zinc-800 outline-none text-white`} placeholder="Search for matching tags" />
+                    <input onChange={(e) => setSearchTerm(e.target.value)} type="text" className={`w-[75%] lg:w-[50%] py-3 md:py-2 px-3 bg-zinc-800 outline-none text-[10px] md:text-sm text-white`} placeholder="Search for tags" />
                     <p onClick={() => setOption(option === 'latest' ? 'oldest' : 'latest')} className={`w-auto px-3 py-1 rounded-full text-gray-400 cursor-pointer select-none capitalize text-center flex justify-center items-center gap-2`}>{option} <FaSort /></p>
                 </div>
 
@@ -109,6 +109,7 @@ function page() {
 
 
 export default page;
+
 
 
 
