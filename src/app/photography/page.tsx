9574bbc552284199ffb081da8760688f93f9aaeb
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2026-03-21 21:23:52 +0530

    Update page.tsx

--- src/app/photography/page.tsx
+++ src/app/photography/page.tsx
@@ -12,7 +12,7 @@ function page() {
     return (
         <>
             <div className={`w-full bg-[#080808] relative h-auto flex flex-col justify-start items-center overflow-hidden`}>
-          <p className={`w-full select-none absolute top-6 text-center text-white font-Red-Hat-Display tracking-[0.3rem] text-sm`}>ISUDIPTODAS</p>
+          <p className={`w-full select-none absolute top-6 text-center text-white font-Red-Hat-Display tracking-[0.3rem] text-[10px] md:text-sm`}>ISUDIPTODAS</p>
                 <div className={`w-full h-auto z-50 flex justify-center items-center px-5 fixed bottom-8`}>
                     <Link href='/' className={`w-auto px-5 py-3 rounded-full text-white font-Red-Hat-Display text-[10px] md:text-sm backdrop-blur-3xl bg-white/25`}>Exit Section</Link>
                 </div>
