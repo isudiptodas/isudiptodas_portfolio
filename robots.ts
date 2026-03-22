6c8792391aaf1f8d06f710b1a01448f01a0d4f82
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2026-03-22 15:47:54 +0530

    Update robots.ts

--- robots.ts
+++ robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://isudiptodas-portfolio.vercel.app/sitemap.xml",
+    sitemap: "https://www.isudiptodas.online/sitemap.xml",
   };
 }
