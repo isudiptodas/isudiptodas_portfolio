0dcfc508439b725887efc24590176009cb8b3f87
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2026-05-28 00:35:12 +0530

    Update documents.ts

--- src/data/documents.ts
+++ src/data/documents.ts
@@ -1,22 +1,22 @@
 export const documentList = [
     {
         name: `Vibe Error`,
-        thumbnail: `/assets/document_thumbnails/vibe-error.png`,
+        thumbnail: `/assets/document-thumbnails/vibe-error.png`,
         link: `/assets/documents/vibe-error.pdf`
     },
     {
         name: `The Art Of Web Development`,
-        thumbnail: `/assets/document_thumbnails/web-development.png`,
+        thumbnail: `/assets/document-thumbnails/web-development.png`,
         link: `/assets/documents/web-dev.pdf`
     },
     {
         name: `Decoding LLM`,
-        thumbnail: `/assets/document_thumbnails/decoding-llm.png`,
+        thumbnail: `/assets/document-thumbnails/decoding-llm.png`,
         link: `/assets/documents/decoding-llm.pdf`
     },
     {
         name: `The Gemini Effect`,
-        thumbnail: `/assets/document_thumbnails/gemini-effect.png`,
+        thumbnail: `/assets/document-thumbnails/gemini-effect.png`,
         link: `/assets/documents/gemini-effect.pdf`
     },
 
