b49db774f81c96a90f3bd4d6a41be0fd753a9d43
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2026-06-17 16:13:23 +0530

    Update photo-design.ts

--- src/data/photo-design.ts
+++ src/data/photo-design.ts
@@ -1,5 +1,4 @@
 export const imageList = [
-  "https://ik.imagekit.io/isudiptodas/photography/messi-algeria-hatrick-fifa26.png",
   "https://ik.imagekit.io/isudiptodas/photography/tram-window.png",
   "https://ik.imagekit.io/isudiptodas/photography/upside-down.jpg",
   "https://ik.imagekit.io/isudiptodas/photography/victoria-afternoon.jpg",
@@ -58,6 +57,7 @@ export const imageList = [
 ];
 
 export const designList = [
+  "https://ik.imagekit.io/isudiptodas/photography/messi-algeria-hatrick-fifa26.png",
   "https://ik.imagekit.io/isudiptodas/design/anirudh-ravichander.png",
   "https://ik.imagekit.io/isudiptodas/design/argentina.png",
   "https://ik.imagekit.io/isudiptodas/design/bahubali-the-epic.png",
