115a1ad1d1e32f4455df954f1dc329ab78a12961
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2026-08-12 01:50:56 +0530

    Update photo-design.ts

--- src/data/photo-design.ts
+++ src/data/photo-design.ts
@@ -44,7 +44,6 @@ export const imageList = [
   "https://ik.imagekit.io/isudiptodas/photography/kumartuli.jpg",
   "https://ik.imagekit.io/isudiptodas/photography/long-exposure.jpg",
   "https://ik.imagekit.io/isudiptodas/photography/maidan-afternoon.jpg",
-  "https://ik.imagekit.io/isudiptodas/photography/maidan-light-trail.jpg",
   "https://ik.imagekit.io/isudiptodas/photography/maidan-morning-2.jpg",
   "https://ik.imagekit.io/isudiptodas/photography/maidan-morning.jpg",
   "https://ik.imagekit.io/isudiptodas/photography/maidan-winter.jpg",
