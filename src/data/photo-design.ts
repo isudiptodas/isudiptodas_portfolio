36bc9f5b4fed24547ec0e7f3d03445f98b8e854c
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2026-09-23 03:42:20 +0530

    Update photo-design.ts

--- src/data/photo-design.ts
+++ src/data/photo-design.ts
@@ -1,6 +1,7 @@
 export const imageList = [
+  "https://ik.imagekit.io/isudiptodas/photography/rush-road-frame.jpg",
   "https://ik.imagekit.io/isudiptodas/photography/sunrays-train-window.jpg",
-  "https://ik.imagekit.io/isudiptodas/photography/kumartuli-7",
+  "https://ik.imagekit.io/isudiptodas/photography/kumartuli-7.jpg",
   "https://ik.imagekit.io/isudiptodas/photography/sudipto-gq.jpg",
   "https://ik.imagekit.io/isudiptodas/photography/radha-krishna.jpg",
   "https://ik.imagekit.io/isudiptodas/photography/argha-vogue-fashion.jpg",
